cd3a0ccc2f8a56dff4fa5ea94052d5ceb6c09c33
vogt4nick <vogt4nick@gmail.com>
2019-12-21 11:50:54 -0500

    set RELATIVE_URLS = True

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ SOCIAL = (
 DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 # theme-based globals
 THEME = "themes/attila"
